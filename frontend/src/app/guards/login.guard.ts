1fe9eb8bb0f6f7d7e32e0d0c73d9679d71e769c6
hendisantika <hendisantika@yahoo.co.id>
2019-08-04 06:34:12 +0700

    #18 - Modify app.component.html file

--- frontend/src/app/guards/login.guard.ts
+++ frontend/src/app/guards/login.guard.ts
@@ -3,4 +3,17 @@ import {Injectable} from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements {}
+export class LoginGuard implements {
+  constructor(private _loginService: LoginServiceService, private _router: Router) {
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
+    if (!this._loginService.getAuthStatus()) {
+      return true;
+    }
+
+    this._router.navigate(['/home']);
+    return false;
+  }
+}
