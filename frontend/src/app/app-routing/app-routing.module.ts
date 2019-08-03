4610ca0e2c56c8d2d7025af8d1d1478fc3daa571
hendisantika <hendisantika@yahoo.co.id>
2019-08-04 06:50:18 +0700

    #20 - Generate app-routing module

---
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from '../components/home/home.component';
+import {LoginComponent} from '../components/login/login.component';
+import {RegisterComponent} from '../components/register/register.component';
+import {AuthGuardGuard} from '../guards/AuthGuard/auth-guard.guard';
+import {LoginGuard} from '../guards/Login/login.guard';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full', canActivate: [LoginGuard]},
+  {path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuardGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [LoginGuard]},
+  {path: '**', redirectTo: '/login', pathMatch: 'full', canActivate: [LoginGuard]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
