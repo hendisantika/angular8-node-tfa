46fb0a452d408da80edf3207d66b4b87d904c8c3
Hendi Santika <hendisantika@gmail.com>
2019-08-06 18:46:30 +0700

    #28 - Modify header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {LoginServiceService} from '../../services/login-service/login-service.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +9,28 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  isLoggedIn: boolean = false;
+
+  constructor(private _loginService: LoginServiceService, private _router: Router) {
+    this._loginService.authSub.subscribe((data) => {
+      this.isLoggedIn = data;
+    });
   }
 
   ngOnInit() {
+    this.isLoggedIn = this._loginService.getAuthStatus();
   }
 
+  toggleMenuBar() {
+    if (document.getElementById('collapsibleNavId').style.display == 'block') {
+      document.getElementById('collapsibleNavId').style.display = 'none';
+    } else {
+      document.getElementById('collapsibleNavId').style.display = 'block';
+    }
+  }
+
+  logout() {
+    this._loginService.logoutUser();
+    this._router.navigate(['/login']);
+  }
 }
