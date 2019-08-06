cc9d9e98d897fae567336ddb748de5fea8556e1a
Hendi Santika <hendisantika@gmail.com>
2019-08-06 18:50:03 +0700

    #31 - Modify register

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {LoginServiceService} from '../../services/login-service/login-service.service';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +9,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {
+  errorMessage: string = null;
+
+  userObject = {
+    uname: '',
+    upass: ''
+  };
+
+  confirmPass: string = '';
+
+  constructor(private _loginService: LoginServiceService, private _router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+    if (this.userObject.uname.trim() !== '' && this.userObject.upass.trim() !== '' && (this.userObject.upass.trim() === this.confirmPass)) {
+      this._loginService.registerUser(this.userObject).subscribe((data) => {
+        const result = data.body;
+        if (result['status'] === 200) {
+          this.errorMessage = result['message'];
+          setTimeout(() => {
+            this._router.navigate(['/login']);
+          }, 2000);
+        }
+      });
+    }
   }
 
 }
